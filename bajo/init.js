c8bf0ce860354b6357d80d1cb9ee64a4b40b16d1
Ardhi Lukianto <ardhi@lukianto.com>
2024-05-02 22:07:21 +0700

    Updates

--- bajo/init.js
+++ bajo/init.js
@@ -1,7 +1,7 @@
 const types = ['datetime', 'date', 'timestamp']
 
 async function handler ({ item }) {
-  const { error } = this.bajo.helper
+  const { error, join } = this.bajo.helper
   const { getSchema } = this.bajoDb.helper
   const { has } = this.bajo.helper._
   for (const f of ['source', 'destination']) {
@@ -11,7 +11,7 @@ async function handler ({ item }) {
     const schema = getSchema(item[f])
     const prop = schema.properties.find(p => p.name === item[key])
     if (!prop) throw error('Unknown field \'%s@%s\'', item[key], item[f])
-    if (!types.includes(prop.type)) throw error('\'%s@%s (%s)\' is not supported (must be one of %s)', item[key], item[f], prop.type, types.join(', '))
+    if (!types.includes(prop.type)) throw error('\'%s@%s (%s)\' is not supported (must be one of %s)', item[key], item[f], prop.type, join(types))
   }
   if (item.source === item.destination) throw error('Source & destination must be different')
   item.maxAge = item.maxAge ?? 1 // in days, less then 1 is ignored
